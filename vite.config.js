5ffc1fd03dea07c80a64d006669fbbbb6930dd75
AnaelleR <naths.raf@gmail.com>
2025-04-21 12:15:25 +0200

    fichier dist

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
- import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
- export default defineConfig({
-   plugins: [react()],
-   build: {
-     rollupOptions: {
-       input: {
-         main: 'index.html',
-         404: 'public/404.html',
-       },
-     },
-   },
- });
+export default defineConfig({
+  base: '/bento_japan/',
+  plugins: [react()],
+})
 
